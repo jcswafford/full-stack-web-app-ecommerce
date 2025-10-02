5649ce361a7a21cc12eb2356cebab73c794c41e8
jswaf20 <jswaf20@wgu.edu>
2025-10-02 12:06:45 -0400

    Updating Angular versions from 17 - 18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 import { ProductCategoryMenuComponent } from "./components/product-category-menu/product-category-menu.component";
 import { SearchComponent } from './components/search/search.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SearchComponent, RouterModule, ProductCategoryMenuComponent, HttpClientModule, ProductCategoryMenuComponent],
+  imports: [RouterOutlet, SearchComponent, RouterModule, ProductCategoryMenuComponent, 
+// TODO: `HttpClientModule` should not be imported into a component directly.
+// Please refactor the code to add `provideHttpClient()` call to the provider list in the
+// application bootstrap logic and remove the `HttpClientModule` import from this component.
+HttpClientModule, ProductCategoryMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
